672f52611c1b504539d0e0af8ff62f8a4a200767
miracleibanze <miraclecode11@gmail.com>
2025-02-10 15:06:59 +0200

    corrected while deploying

--- client/app/dashboard/challenges&hackathons/create-new-challenge/page.tsx
+++ client/app/dashboard/challenges&hackathons/create-new-challenge/page.tsx
@@ -25,7 +25,6 @@ const Page: FC = () => {
     description: "",
     duration: "",
     prize: "",
-    contact: "",
     participants: [],
     projectRequirements: [],
     projectDesign: [],
--- client/app/dashboard/talents/[userId]/page.tsx
+++ client/app/dashboard/talents/[userId]/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import Profile from "@app/dashboard/profile/page";
 import { useSelector } from "react-redux";
 import { RootState } from "@redux/store";
 import { useParams } from "next/navigation";
+import ProfileComponent from "@components/ProfileComponent";
 
 const page = () => {
   const { talents } = useSelector((state: RootState) => state.talent);
   const { userId } = useParams();
   const selectedUser = talents.find((item) => item._id === userId)!;
-  return <Profile selectedUser={selectedUser} />;
+  return <ProfileComponent user={selectedUser} />;
 };
 
 export default page;
