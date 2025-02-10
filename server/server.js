bd07531f473ad347e9eb1bafb59c41a189a16a1a
miracleibanze <miraclecode11@gmail.com>
2025-02-10 20:23:18 +0200

    corrected while deploying

--- server/server.js
+++ server/server.js
@@ -28,7 +28,10 @@ app.use("/api/", userRoutes);
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
